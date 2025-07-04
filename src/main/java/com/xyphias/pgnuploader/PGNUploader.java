726cd192570a8af9e5419286bbf11567d6ed71f1
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-07-04 09:05:14 +0100

    Extract a method to keep only the paths to events and games

--- src/main/java/com/xyphias/pgnuploader/PGNUploader.java
+++ src/main/java/com/xyphias/pgnuploader/PGNUploader.java
@@ -3,9 +3,14 @@ import static java.lang.Integer.MAX_VALUE;
 void main() throws IOException {
     var root = readln("Enter the root directory: ");
     
-    try (var paths = Files.walk(Path.of(root), MAX_VALUE, FileVisitOption.FOLLOW_LINKS)) {
-        paths
-                .filter(path -> !path.toString().contains(".git"))
-                .forEach(path -> println(path.toString()));
+    try (var paths = pathsToEventsAndGamesIn(root)) {
+        paths.forEach(path -> println(path.toString()));
     }
+}
+
+private static Stream<Path> pathsToEventsAndGamesIn(String root) throws IOException {
+    return 
+            Files.walk(Path.of(root), MAX_VALUE, FileVisitOption.FOLLOW_LINKS)
+                    .skip(1)
+                    .filter(path -> !path.toString().contains(".git"));
 }
