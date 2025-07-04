a46a831e6bbb501743418879d2f137c7a1f13180
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-07-04 09:19:33 +0100

    Move try-with-resources into pathsToEventsAndGamesIn and make that method return a List instead of a Stream

--- src/main/java/com/xyphias/pgnuploader/PGNUploader.java
+++ src/main/java/com/xyphias/pgnuploader/PGNUploader.java
@@ -3,14 +3,16 @@ import static java.lang.Integer.MAX_VALUE;
 void main() throws IOException {
     var root = readln("Enter the root directory: ");
     
-    try (var paths = pathsToEventsAndGamesIn(root)) {
-        paths.forEach(path -> println(path.toString()));
-    }
+    var paths = pathsToEventsAndGamesIn(root);
+    
+    paths.forEach(path -> println(path.toString()));
 }
 
-private static Stream<Path> pathsToEventsAndGamesIn(String root) throws IOException {
-    return 
-            Files.walk(Path.of(root), MAX_VALUE, FileVisitOption.FOLLOW_LINKS)
-                    .skip(1)
-                    .filter(path -> !path.toString().contains(".git"));
+private static List<Path> pathsToEventsAndGamesIn(String root) throws IOException {
+    try (var paths = Files.walk(Path.of(root), MAX_VALUE, FileVisitOption.FOLLOW_LINKS)) {
+        return paths
+                .skip(1)
+                .filter(path -> !path.toString().contains(".git"))
+                .toList();
+    }
 }
