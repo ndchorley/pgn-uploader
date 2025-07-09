c1d29b63d51fec4b9f7eb20393886518a384b3c8
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-07-09 07:39:36 +0100

    Refactor to introduce getAFileIn

--- src/main/java/com/xyphias/pgnuploader/PGNUploader.java
+++ src/main/java/com/xyphias/pgnuploader/PGNUploader.java
@@ -9,19 +9,36 @@ import static java.lang.Integer.MAX_VALUE;
 
 void main() throws Exception {
     var root = readln("Enter the root directory: ").trim();
-    
+
     var paths = pathsToEventsAndGamesIn(root);
 
     var events = eventsFrom(paths);
     println(events);
-    
+
     Drive drive = buildDrive();
+
+    var aFileInTheDrive = getAFileIn(drive);
+
+    println(aFileInTheDrive);
+}
+
+private static com.google.api.services.drive.model.File getAFileIn(Drive drive)
+        throws IOException {
+    var files =
+            drive
+                    .files()
+                    .list()
+                    .execute()
+                    .getFiles();
     
-    println(drive.files().list().execute().getFiles().getFirst());
+    if (files.isEmpty())
+        return null;
+    
+    return files.getFirst();
 }
 
 private static List<Path> pathsToEventsAndGamesIn(String root) throws IOException {
-    try (var paths = 
+    try (var paths =
                  Files.walk(Path.of(root), MAX_VALUE, FileVisitOption.FOLLOW_LINKS)
     ) {
         return paths
@@ -42,13 +59,13 @@ private List<String> eventsFrom(List<Path> paths) {
 
 private String eventName(File file) {
     var parts = file.getAbsolutePath().split("/");
-    
+
     return parts[parts.length - 1];
 }
 
 private Drive buildDrive() throws Exception {
     String fileName = System.getenv("CREDENTIALS_FILE");
-    
+
     var credentials = credentialsFrom(fileName);
 
     var builder =
@@ -57,13 +74,13 @@ private Drive buildDrive() throws Exception {
                     GsonFactory.getDefaultInstance(),
                     new HttpCredentialsAdapter(credentials)
             );
-    
+
     return builder.setApplicationName("PGN Uploader").build();
 }
 
 private GoogleCredentials credentialsFrom(String fileName) throws Exception {
     var credentialFileStream = getClass().getResourceAsStream(fileName);
-    
+
     return GoogleCredentials
             .fromStream(credentialFileStream)
             .createScoped(DriveScopes.DRIVE_METADATA_READONLY);
